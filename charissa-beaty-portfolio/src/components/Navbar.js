420f95d07fd24cfd53de8776181ef3516d2c56ad
Charissa Beaty <char.beaty11@gmail.com>
2020-02-05 19:50:28 -0700

    made navbar transparent, added logo

--- charissa-beaty-portfolio/src/components/Navbar.js
+++ charissa-beaty-portfolio/src/components/Navbar.js
@@ -3,22 +3,33 @@ import React from "react";
 const Navbar = () => {
     return (
         <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark" id="navbarHome">
-    <div className="container">
-    <ul className="nav navbar-na">
-      <li className="nav-item">
-        <a className="nav-link" href="#">About <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Portfolio</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Contact</a>
-      </li>
-    </ul>
+ <nav className="navbar navbar-expand-lg navbar-transparent static-top">
+  <div className="container">
+    <a className="navbar-brand" href="#"><img src="./images/myLogo.png" width="120" height="120" alt="myLogo"></img></a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+    <div className="collapse navbar-collapse" id="navbarResponsive">
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item active">
+          <a className="nav-link" href="#">Home
+                <span className="sr-only">(current)</span>
+              </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Portfolio</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
-        </div>
+</div>
     );
 };
 
